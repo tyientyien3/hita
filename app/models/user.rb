3194a353e8c2968cbaf5886cd78e93fa165f2c93
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-01 05:01:47 +0000

    istall bootstrap and devise

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum user_status: {
+    "日本在住": 0,
+    "日本在住（海外経験あり）": 1,
+    "海外在住": 2,
+  }
+end
