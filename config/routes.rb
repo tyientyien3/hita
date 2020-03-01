e939c0320eb007ef57901ad46add3df3639f7cdb
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-01 05:39:13 +0000

    make controllers and set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+  #ルート設定
   root "homes#index"
 
+  #devise
   devise_for :users
+
+  #国/都市/地域 routing
+  resources :countries, only: [:show]
+  resources :regions, only: [:show]
+  resources :areas, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get questions_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get questions_show_url
+    assert_response :success
+  end
+
+end
