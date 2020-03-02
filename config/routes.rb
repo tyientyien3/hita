0a8d111861d1a112de32b4e874111b96ffb10a1a
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-02 07:20:06 +0000

    editing user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   #ルート設定
   root "homes#index"
-
   #devise
   devise_for :users
-
+  #users routing
+  resources :users, only: [:show, :edit, :update, :destroy]
   #国/都市/地域 routing
   resources :countries, only: [:show]
   resources :regions, only: [:show]
