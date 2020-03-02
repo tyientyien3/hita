1d5e5c9b8d57e9f253311ec54c51becbba3a6b9e
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-02 05:41:39 +0000

    edit:country show page

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,9 @@
 class CountriesController < ApplicationController
+
+  #　メンターさんに質問
+  #　書き方あってる？特定のcountry_id を持ってるregionが選ばれる。
   def show
+    @country = Country.find(params[:id])
+    @regions = Region.where(country_id: params[:id])
   end
 end
