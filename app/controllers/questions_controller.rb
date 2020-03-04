94c5b8828b38677ff3d4375a0b654b920d6cd1d7
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-04 12:23:03 +0000

    edit controller new/create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,32 @@
 class QuestionsController < ApplicationController
   def new
+    @user = User.find(params[:user_id])
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @user = User.find(params[:user_id])
+    #質問者のuser_id
+    @question.questioner = current_user
+    #回答者のuser_id
+    @question.questionee = User.find(params[:user_id])
+    if @question.save
+      flash[:success] = "Book was successfully created."
+      # binding.pry
+      redirect_to _path(@question.questionee_id, @question)
+    else
+      render :new
+    end
   end
 
   def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question, :answer)
   end
 end
