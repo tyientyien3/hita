eb9853281eaf8ad1d1bf02b008f02599368c9a06
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-09 12:19:32 +0000

    ップページ下段編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,15 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @countries = []
-    @regions = []
-    Country.all.each do |country|
-      @countries.push(country.country_name)
-      @temp = []
-      country.regions.each do |region|
-        @temp.push(region.region_name)
-      end
-      @regions.push(@temp)
-    end
+    @countries = Country.all
   end
 
   def about
