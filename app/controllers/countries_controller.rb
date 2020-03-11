d1102f5ffe883e352b2c32eb8cdd055a9af6ce53
tyientyien3 <kunchikitaforp@gmail.com>
2020-03-11 04:13:35 +0000

    country add pagination

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,8 +3,10 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
-    @questions = @country.questions
-    @users = @country.users
+    questions = @country.questions
+    @pagenated_questions = questions.page(params[:page]).per(6)
+    users = @country.users
+    @pagenated_users = users.page(params[:page]).per(1)
     @regions = Region.where(country_id: params[:id])
   end
 end
